d41fee0cc7974d536d458de27c41a478b0c8a056
visionsmika-gif <visionsmika@gmail.com>
2025-12-08 19:23:11 -0800

    update

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <list>
 #include <cstdlib>
 #include <ctime>
+#include <deque>
 
 using namespace std;
 
@@ -19,6 +20,9 @@ const string NAMES[NUM_NAMES] = { "Liam", "Olivia", "Jasper", "Delilah", "Theodo
 const int NUM_COFFEES = 5;
 const string COFFEE_ORDERS[NUM_COFFEES] = { "Latte", "Cappuccino", "Mocha", "Americano", "Espresso" };
 
+const int NUM_MUFFINS = 5;
+const string MUFFIN_ORDERS[NUM_MUFFINS] = { "Blueberry", "Banana", "Apple", "Lemon", "Cranberry" };
+
 // Coffee node
 struct CoffeeNode {
 	string name;
@@ -27,25 +31,51 @@ struct CoffeeNode {
 	CoffeeNode(const string& n, const string& o) : name(n), order(o) {}
 };
 
+// Muffin node
+struct MuffinNode {
+	string name;
+	string order;
+
+	MuffinNode(const string& n, const string& o) : name(n), order(o) {}
+};
+
+// Customer joins coffee booth
 void customerJoins(list<CoffeeNode>& coffeeBooth) {
 	string randomName = NAMES[rand() % NUM_NAMES];
 	string randomOrder = COFFEE_ORDERS[rand() % NUM_COFFEES];
 	coffeeBooth.push_back(CoffeeNode(randomName, randomOrder));
-	cout << "\tCustomer Joins: " << coffeeBooth.back().name << " - " << coffeeBooth.back().order << "\n";
+	cout << "\tCustomer joins: " << coffeeBooth.back().name << " - " << coffeeBooth.back().order << "\n";
+}
+
+// Customer joins muffin booth
+void customerJoins(deque<MuffinNode>& muffinBooth) {
+	string randomName = NAMES[rand() % NUM_NAMES];
+	string randomOrder = MUFFIN_ORDERS[rand() % NUM_MUFFINS];
+	muffinBooth.push_back(MuffinNode(randomName, randomOrder));
+	cout << "\tCustomer joins: " << muffinBooth.back().name << " - " << muffinBooth.back().order << "\n";
 }
 
 int main() {
 	srand(time(0));
-	list<CoffeeNode> coffeeBooth;	// Linked list for coffee orders.
+	const int NUM_BOOTHS = 2;
+	list<CoffeeNode> coffeeBooth;	// Linked list for coffee booth.
+	deque<MuffinNode> muffinBooth;	// Deque for muffin booth.
 
 	// Initialize the queue with 3 customers.
-	cout << "INITIAL COFFEE QUEUE:\n";
-	for (int i = 0; i < 3; ++i) {
+	const int INIT_NUM_CUSTOMERS = 3;
+
+	cout << "INITIAL COFFEE BOOTH:\n";
+	for (int i = 0; i < INIT_NUM_CUSTOMERS; ++i) {
 		customerJoins(coffeeBooth);
 	}
 
+	cout << "INITIAL MUFFIN BOOTH:\n";
+	for (int i = 0; i < INIT_NUM_CUSTOMERS; ++i) {
+		customerJoins(muffinBooth);
+	}
+
 	// Run 10 rounds of simulation.
-	cout << "COFFEE BOOTH SIMULATION:\n";
+	cout << "BOOTH SIMULATIONS:\n";
 	for (int i = 0; i < NUM_ROUNDS; ++i) {
 		cout << "Time " << i + 1 << ":\n";
 
