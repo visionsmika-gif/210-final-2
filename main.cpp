1a894545d382736c0fa455da73700badbb3f12fb
visionsmika-gif <visionsmika@gmail.com>
2025-12-08 18:47:11 -0800

    update

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 // IDE used: Visual Studio 2022
 
 #include <iostream>
+#include <string>
+#include <list>
 
 using namespace std;
 
+// Data sets
+const string NAMES[] = { "Liam", "Olivia", "Jasper", "Delilah", "Theodore", "Amelia", "Noah", "Emma", "Knox", "Sloane",
+							 "Zoe", "Flynn", "Saoirse", "Maxton", "Scarlett", "Lucian", "Aisha", "Kairi", "Zariah", "Nova" };
+
+const string COFFEE_ORDERS[] = { "Latte", "Cappuccino", "Mocha", "Americano", "Espresso" };
+
+// Coffee node
+struct CoffeeNode {
+	string name;
+	string order;
+};
+
 int main() {
+	list<CoffeeNode> coffeeBooth;	// Linked list
+
 
 	return 0;
 }
